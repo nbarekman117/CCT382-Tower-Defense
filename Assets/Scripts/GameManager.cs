5777e99b53bdfd3f2c2e3ebbe7fd3c15911801f4
nbarekman117 <162934549+nbarekman117@users.noreply.github.com>
2024-12-15 14:28:44 -0500

    commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public Spawner spawner;
+    public HealthSystem health;
+    public CurrencySystem currency;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        health.Init();
+        currency.Init();
+        StartCoroutine(WaveStartDelay());
+    }
+
+    IEnumerator WaveStartDelay()
+    {
+        yield return new WaitForSeconds(2f);
+    }
+}
